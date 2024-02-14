a08aff031fb9626a7624b4b05817ebb6c1604d3a
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-14 10:57:29 -0600

    Added ordersList to render function, and added event listener that calls render whenever an order is placed

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 import { interiorOptions } from "./interiorOptions.js"
 import { orderButton } from "./orderButton.js"
+import { ordersList } from "./ordersList.js"
 import { paintOptions } from "./paintOptions.js"
 import { technologyOptions } from "./technologyOptions.js"
 import { wheelOptions } from "./wheelOptions.js"
@@ -10,27 +11,30 @@ const render = async () => {
     const technologyHTML = await technologyOptions()
     const wheelHTML = await wheelOptions()
     const buttonHTML = await orderButton()
+    const ordersHTML = await ordersList()
     
     const composedHTML = `
     <h1>Cars 'R Us: Personal Car Builder</h1>
 
-    <article class="choices">
+    <article id="choices">
         ${paintHTML}
         ${interiorHTML}
         ${technologyHTML}
         ${wheelHTML}
     </article>
 
-    <article class="order">
+    <article id="orderButton">
         ${buttonHTML}
     </article>
 
-    <article class="customOrders">
-    <h2>Custom Car Orders</h2>
+    <article id="customOrders">
+        ${ordersHTML}
     </article>
     `
     const mainHTML = document.querySelector("#container")
     mainHTML.innerHTML = composedHTML
 }
+
+document.addEventListener("newOrderPlaced", render)
 
 render()
