8e0a7e33fab26f87cd2d49a6442d021a2b315f7f
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-02-18 16:37:58 -0600

    Added HTML generator for vehicle style options

---
+++ scripts/styleOptions.js
@@ -0,0 +1,16 @@
+export const styleOptions = async () => {
+    const response = await fetch("http://localhost:8088/styles")
+    const styles = await response.json()
+
+    let stylesHTML = `<section class="optionCard"><h2>Vehicle Style</h2><select id=style>`
+
+    stylesHTML +=`<option value="0" selected disabled hidden>Please Select a Vehicle Style</option>`
+
+    const optionStringArray = styles.map(style => `<option value="${style.id}">${style.style}</option>`)
+
+    stylesHTML += optionStringArray.join("")
+    
+    stylesHTML += "</select></section>"
+
+    return stylesHTML
+}
